973c88b1183f8ca74bb5ad0346c5a1a0da99f934
Reina <r.clarkemitchell@gmail.com>
2019-09-22 12:58:20 -0400

    Done.

--- app/models/startup.rb
+++ app/models/startup.rb
@@ -1,3 +1,59 @@
 class Startup
+    @@all = []
+    attr_reader :founder, :domain
+    attr_accessor :name
+    def initialize(name, founder, domain)
+        @name = name
+        @founder = founder
+        @domain = domain        
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def sign_contract(venture_capitalist, type, amount)
+        new_contract = FundingRound.new(venture_capitalist,self,type, amount) #venture_capitalist, startup and type are the params for my Funding Round class
+        # new_contract.investment = amount
+        new_contract
+    end
+
+    def pivot(domain, name)
+        @domain = domain
+        @name = name
+    end
+
+    def funding_rounds
+       FundingRound.all.select {|funding| funding.startup == self} 
+    end
+
+    def num_funding_rounds
+        self.funding_rounds.length
+    end
+    
+    def self.find_by_founder(founder_name)
+        self.all.find {|startup| startup.founder == founder_name}
+    end
+
+    def total_funds
+        self.funding_rounds.map {|funding| funding.investment}.reduce(0) {|sum, money| sum + money}
+    end
+
+    def self.domain
+        self.all.map {|startup| startup.domain}
+    end
+
+    def all_investors
+        self.funding_rounds.map {|funding| funding.venture_capitalist}
+    end
+
+    def investors
+        self.funding_rounds.map {|funding| funding.venture_capitalist}.uniq
+    end
+
+    def big_investors
+        self.all_investors.select {|investors| investors.networth.tres_commas_club}
+    end
 
 end
--- app/models/venture_capitalist.rb
+++ app/models/venture_capitalist.rb
@@ -1,3 +1,45 @@
-class VentureCapitalist
+class Venture_capitalist
+    
+    @@all =[]
+    attr_reader :name, :total_worth, :investment
+    attr_accessor  :type
+    def initialize(name, networth)
+        @name = name
+        @total_worth = networth
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def offer_contract(startup, type,amount)
+        new_contract = FundingRound.new(self,startup,type, amount)
+        # new_contract.investment = amount
+        new_contract
+    end
+
+
+    def funding_rounds
+        FundingRound.all.select {|round| round.venture_capitalist == self}
+    end
+    
+    def self.tres_commas_club
+        self.all.select {|vc| vc.networth > 1000000000}
+        
+    end
+
+    def portfolio
+        self.funding_rounds.map {|funding| funding.startup.name}
+    end
+
+    def biggest_investment
+        self.funding_rounds.max_by {|investor| investor.investment}
+    end
+
+    def invested(domain_string)
+     domain_check = self.funding_rounds.select {|funding| funding.startup.domain == domain_string}
+     domain_check.reduce(0) {|sum, money| sum + money.investment}
+    end
 
 end
